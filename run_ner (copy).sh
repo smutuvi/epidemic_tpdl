e11bafaf2ef0593962c75cc106458000e773c85f
steve mutuvi <stevemutuvi@gmail.com>
2021-03-19 08:43:54 +0300

    run_ner.sh updated

--- run_ner (copy).sh
+++ run_ner (copy).sh
@@ -1,11 +1,7 @@
 export MAX_LENGTH=128
-export BERT_MODEL=bert-base-multilingual-cased
-python3 preprocess.py train.txt.tmp $BERT_MODEL $MAX_LENGTH > data/train.txt
-python3 preprocess.py dev.txt.tmp $BERT_MODEL $MAX_LENGTH > data/dev.txt
-python3 preprocess.py test.txt.tmp $BERT_MODEL $MAX_LENGTH > data/test.txt
-#cat train.txt dev.txt test.txt | cut -d " " -f 2 | grep -v "^$"| sort | uniq > data/labels.txt
+export BERT_MODEL=xlm-roberta-large
 export OUTPUT_DIR=output_dir
-export BATCH_SIZE=32
+export BATCH_SIZE=16
 export NUM_EPOCHS=3
 export SAVE_STEPS=750
 export SEED=1
